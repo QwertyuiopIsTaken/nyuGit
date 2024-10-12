d75802a46b2801d44882297d52a7adc2dc814ff2
QwertyuiopIsTaken <ricky85125@gmail.com>
2024-10-11 22:41:03 -0400

    finished homework 6

--- homework/CountOccurence.java
+++ homework/CountOccurence.java
@@ -6,29 +6,39 @@ The purpose of this program is to count the occurrence of numbers in a list of i
 
 // Imports
 import java.util.Scanner; // reading inputs
-import java.util.ArrayList; // useful because the size of arraylist can be increased unlike regular arrays
+import java.util.ArrayList;
 
 public class CountOccurrence {
 	public static void main(String[] args) {
 		// Declaration
-		ArrayList<int> inputs = new ArrayList<int>();
+		ArrayList<Integer> inputs = new ArrayList<Integer>(); // allows for expandable array size
 		int enteredInt;
 		Scanner numInput = new Scanner(System.in);
-		int[] counts = new int[100]
+		int[] counts = new int[100];
 
 		// Input
 		System.out.println("Enter integers between 1 and 100 (enter 0 to stop): ");
 		enteredInt = numInput.nextInt();
 		while (enteredInt != 0) {
 			inputs.add(enteredInt);
+			enteredInt = numInput.nextInt(); // continues prompting for inputs until 0 is entered
 		}
 
 		// Processing
 		for (int i = 0; i < inputs.size(); i++) {
-			counts[inputs.get(i)] += 1;
+			counts[inputs.get(i) - 1] += 1; // all the indexes are shifted one to the left
 		}
 
 		// Output
-		System.out.println(Arrays.toString(counts));
+		for (int i = 0; i < counts.length; i++) {
+			if (counts[i] > 0) {
+				System.out.print("The number " + (i + 1) + " occurred " + counts[i]);
+				if (counts[i] > 1) {
+					System.out.println(" times.");
+				} else {
+					System.out.println(" time.");
+				}
+			}
+		}
 	}
 }
